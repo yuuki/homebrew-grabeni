a5fa4fb353a3627579fe32801015da00b7088df9
y_uuki <yuki.tsubo@gmail.com>
2016-03-13 15:13:14 +0900

    Rename github account

--- grabeni.rb
+++ grabeni.rb
@@ -1,18 +1,18 @@
 class Grabeni < Formula
   grabeni_version = '0.4.1'
-  homepage 'https://github.com/yuuki1/grabeni'
+  homepage 'https://github.com/yuuki/grabeni'
   version grabeni_version
 
   if Hardware.is_64_bit?
-    url "https://github.com/yuuki1/grabeni/releases/download/v#{grabeni_version}/grabeni_darwin_amd64.zip"
+    url "https://github.com/yuuki/grabeni/releases/download/v#{grabeni_version}/grabeni_darwin_amd64.zip"
     sha256 '628507fffbd8895acc2e00b2ab2a8c7f970a3dad3775a8517bb452616ff024af'
   else
-    url "https://github.com/yuuki1/grabeni/releases/download/v#{grabeni_version}/grabeni_darwin_386.zip"
+    url "https://github.com/yuuki/grabeni/releases/download/v#{grabeni_version}/grabeni_darwin_386.zip"
     sha256 '2010472e4e4a178d6c565442190c627f3c5944f33089f659affcc0c84a5e889c'
   end
 
   head do
-    url 'https://github.com/yuuki1/grabeni.git'
+    url 'https://github.com/yuuki/grabeni.git'
     depends_on 'go' => :build
     depends_on 'git' => :build
   end
@@ -20,8 +20,8 @@ class Grabeni < Formula
   def install
     if build.head?
       ENV['GOPATH'] = buildpath/'.go'
-      mkdir_p buildpath/'.go/src/github.com/yuuki1'
-      ln_s buildpath, buildpath/'.go/src/github.com/yuuki1/grabeni'
+      mkdir_p buildpath/'.go/src/github.com/yuuki'
+      ln_s buildpath, buildpath/'.go/src/github.com/yuuki/grabeni'
       system 'make', 'build'
     end
     bin.install 'grabeni'
